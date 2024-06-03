9de68189b57bb710c11b9d6334f55eae59861fa1
Thonwelling <thonwellingdani13@gmail.com>
2024-06-03 10:25:31 -0300

    Project Created

---
+++ src/main/java/br/com/ambarx/RedirectionServerWhiteLIst/RedirectionServerWhiteLIstApplication.java
@@ -0,0 +1,13 @@
+package br.com.ambarx.RedirectionServerWhiteLIst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectionServerWhiteLIstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectionServerWhiteLIstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ambarx/RedirectionServerWhiteLIst/RedirectionServerWhiteLIstApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ambarx.RedirectionServerWhiteLIst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectionServerWhiteLIstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
